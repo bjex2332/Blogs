1982960b6ce80b5fbb75565c46dfa7ef8e775789
Brandon <bjex2332@gmail.com>
2022-11-17 18:35:09 -0700

    Heroku quick fix

--- controllers/api/trackerAPIRoutes.js
+++ controllers/api/trackerAPIRoutes.js
@@ -28,23 +28,23 @@ router.post('/', (req, res) => {
     });
   });
 
-  router.delete('/:id', withAuth, async (req, res) => {
-    try {
-      const [affectedRows] = Post.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
+  // router.delete('/:id', withAuth, async (req, res) => {
+  //   try {
+  //     const [affectedRows] = Post.destroy({
+  //       where: {
+  //         id: req.params.id,
+  //       },
+  //     });
   
-      if (affectedRows > 0) {
-        res.status(200).end();
-      } else {
-        res.status(404).end();
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  //     if (affectedRows > 0) {
+  //       res.status(200).end();
+  //     } else {
+  //       res.status(404).end();
+  //     }
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // });
 
 
   module.exports = router;
